2c4a752232e567dea417307caff445d162c7d07c
ZC <1725101736@qq.com>
2017-09-05 22:21:35 +0800

    add two numbers

---
+++ exercise/Add Two Numbers.cpp
@@ -0,0 +1,22 @@
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+class Solution{
+public:
+    ListNode* addTownumbers(ListNode *l1,ListNode *l2){
+      ListNode prehead(0);
+      ListNode *p=&prehead;
+      int extra=0;
+      while (l1||l2||extra){
+        int sum=(l1?l1->val:0)+(l2?l2->val:0)+extra;
+        extra=sum/10;
+        p->next=new ListNode(sum%10);
+        p=p->next;
+        l1=l1?l1->next:l1;
+        l2=l2?l2->next:l2;
+      }
+      return &prehead.next;
+    }
+};
